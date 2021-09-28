b65fb990762cf260f9c3a6a776b2e20f5fb1117d
aadilMohd <mohd.aadil2602@gmail.com>
2021-09-28 22:51:38 +0530

    login and signup added

--- app/src/main/java/com/example/hope/Login.java
+++ app/src/main/java/com/example/hope/Login.java
@@ -2,14 +2,54 @@ package com.example.hope;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+    private EditText email;
+    private EditText pass;
+    private TextView singupl;
+    private View signinb;
+    private String emails;
+    private String passs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
-        System.out.println(35);
+        email = findViewById(R.id.emaile);
+        pass = findViewById(R.id.passin);
+        singupl=findViewById(R.id.signinlink);
+        signinb=findViewById(R.id.signinbut);
+
+
+        signinb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                emails=email.getText().toString();
+
+                System.out.println(email);
+
+                startActivity(new Intent(Login.this,MainActivity.class));
+
+
+            }
+
+
+        });
+
+        singupl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                startActivity(new Intent(Login.this,Signup.class));
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/hope/Signup.java
+++ app/src/main/java/com/example/hope/Signup.java
@@ -2,13 +2,36 @@ package com.example.hope;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Signup extends AppCompatActivity {
+
+    private View signup;
+    private TextView signinl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        signup = findViewById(R.id.signupbut);
+        signinl=findViewById(R.id.signinlink);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Signup.this,MainActivity.class));
+            }
+        });
+
+        signinl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Signup.this,Login.class));
+            }
+        });
     }
 }
