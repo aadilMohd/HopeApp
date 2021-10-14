6b91b6c08871751a83096eef9938c78198dafe3c
aadilMohd <mohd.aadil2602@gmail.com>
2021-10-14 14:06:54 +0530

    initialized firestore

--- app/src/main/java/com/example/hope/Signup.java
+++ app/src/main/java/com/example/hope/Signup.java
@@ -1,21 +1,24 @@
 package com.example.hope;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Signup extends AppCompatActivity {
 
@@ -24,6 +27,7 @@ public class Signup extends AppCompatActivity {
     private TextView Fname,Phone,email,pass,pass1;
     private String Fnames,Phones,emails,passs,pass1s;
     private FirebaseAuth mAuth;
+    FirebaseFirestore db ;
 
 
     @Override
@@ -38,6 +42,7 @@ public class Signup extends AppCompatActivity {
         email = findViewById(R.id.emaile);
         pass= findViewById(R.id.pass1e);
         pass1 = findViewById(R.id.confirmpass1);
+        db = FirebaseFirestore.getInstance();
 
 
         signup.setOnClickListener(new View.OnClickListener() {
@@ -80,6 +85,25 @@ public class Signup extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()){
+
+                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                            Map<String, Object> users = new HashMap<>();
+                            assert user != null;
+
+                            users.put("fullname", Fnames);
+                            users.put("phone",Phones);
+                            users.put("email", emails);
+
+                            db.collection("users")
+                                    .document(user.getUid()).set(users).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(Signup.this, "Sucess", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
 
                             startActivity(new Intent(Signup.this,MainActivity.class));
 
